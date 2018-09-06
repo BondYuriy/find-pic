dd787ced6ffd7b347187d4ee6ccc47061bad2dc4
BondYuriy <bondar.y@i.ua>
2018-09-06 12:04:48 +0300

    adds markup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-import './sass/styles.scss';
 import Model from './js/model';
 import View from './js/view';
 import Controller from './js/controller';
+import './sass/styles.scss';
+// import 'normalize.css';
 
 const model = new Model();
 const view = new View();
